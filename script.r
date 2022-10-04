a83aabf75a52e33ad1c2036b95414f20f5a3af98
gv-sh <gv-sh@outlook.com>
2022-10-04 14:58:06 +0530

    bs4

--- script.r
+++ script.r
@@ -1,13 +1,12 @@
 library(ggplot2)
-batch <- read.csv("csv/batch_test.csv",header = TRUE, sep = ",")
+batch <- read.csv("csv/batch_gsdb-2048.csv",header = TRUE, sep = ",")
 batch
 
 ggplot(batch) + 
-  geom_point(alpha = 0.25, size=2, aes(x=dig, y=prb, color=ref)) + 
+  geom_point(alpha=0.25, size=2, aes(x=dig, y=prb)) + 
   geom_line(aes(x=dig, y=prb, color=ref), alpha = 0.25) +
   geom_line(aes(x=dig, y=ben), color="blue") + 
   ylim(0,40) +
-  scale_x_continuous(breaks = seq(1, 10)) +
-  theme_linedraw() + 
-  theme(legend.position="none")
-
+  scale_x_continuous(breaks=seq(1, 10)) +
+  theme(legend.position="none") +
+  theme_linedraw()
